6fb65a2b4df1005c567b83fcef8f9915e2985a10
irving <sanchez.irving006@gmail.com>
2018-09-17 18:47:17 -0600

    shell pipe and pickler as well as exec.py

--- shell/shell.py
+++ shell/shell.py
@@ -1,4 +1,4 @@
-import os, sys, time, re
+import os, sys, time, re, pickle
 
 r, w = os.pipe()
 rc = os.fork()
@@ -6,7 +6,18 @@ if rc < 0:
     os.write(2, ("fork failed, returning %d\n" % rc).encode())
     sys.exit(1)
 
+elif rc == 0:
+    os.execve(os.getcwd(), exec.py)
 
-while 0:
+else:  # parent (forked ok)
     INP = input("$ ")
+    INP = INP.strip()
+    List = list(INP.split(" "))
+    for i in range(len(List)):
+        List[i] = List[i].lower()
+    with open('outfile', 'wb') as fp:
+        pickle.dump(list, fp)
+    childPidCode = os.wait()
+
+
 
